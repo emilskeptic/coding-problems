e382162c5bd3a7946fb08dca2553671c2331610f
emilskeptic <emilskeptic@users.noreply.github.com>
2017-03-04 14:18:29 +0100

    initial commit

---
+++ ninety-nine/lists/problem_06.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+
+# Name:             problem_05.py
+# Purpose:          List palindrome checker.
+# Author:           github.com/emilskeptic
+# Last modified:    2017-03-04
+# Version:          1.0
+# Depends on:       Python 3.5+
+
+def list_palindrome_checker(my_list):
+    if my_list == my_list[::-1]:
+        return True
+    else:
+        return False
+
+foo = [1, 2, 3, 4, 5, 6]
+bar = [1, 2, 3, 3, 2, 1]
+
+print(list_palindrome_checker(foo))
+print(list_palindrome_checker(bar))
